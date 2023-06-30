5acae1baba6e2d8099a79de7133ff0ed52cca312
adnaanrahman08 <adnaanrahman08@gmail.com>
2023-06-30 01:46:37 +0100

    merged commits

--- app/public/canvas.js
+++ app/public/canvas.js
@@ -79,7 +79,7 @@ function done() {
 
   // Display the adjusted area on the screen
   const adjustedAreaDisplay = document.getElementById("areaDisplay");
-  adjustedAreaDisplay.textContent = "Body Surface Area: " + adjustedArea.toFixed(2) + " m\u00B2";
+  adjustedAreaDisplay.textContent = "Body Surface Area: " + adjustedArea.toFixed(2) + " cm\u00B2";
 
 
   // Add the shape area to the table
@@ -190,7 +190,7 @@ function isPointInsideBody(x, y) {
   const blue = pixelData[2];
   const alpha = pixelData[3];
 
-  if (!(red === 0 && green === 0 && blue === 0 && alpha === 0)) { // grey
+  if (!(red === 0 && green === 0 && blue === 0 && alpha === 0)) {
     return true;
   } else {
     console.log(`Background Color: rgba(${red}, ${green}, ${blue}, ${alpha})`);
@@ -322,4 +322,3 @@ function clearPolygons() {
   const shapeTable = document.getElementById("shapeTable");
   shapeTable.style.display = "none";
 }
-
