6cb74de9b5828d0c0b75b384a972e872906809c0
Marco Nocerino <marconocerinoo@gmail.com>
2023-07-10 12:44:22 +0100

    edit calculation and sizing

--- app/public/canvas.js
+++ app/public/canvas.js
@@ -27,7 +27,13 @@ let mask;
 let brushColor = "#FFC867";
 let brushSize = 50;
 let coloredPixels = 0;
-let totalPixels = 0;
+
+let bodyParts = [
+  { name: 'Arms', color: [255, 226, 209], coloredPixels: 0, totalPixels: 0, originalTotalPixels: 0 },
+  { name: 'Head', color: [214, 131, 131], coloredPixels: 0, totalPixels: 0, originalTotalPixels: 0 },
+  { name: 'Legs', color: [202, 196, 206], coloredPixels: 0, totalPixels: 0, originalTotalPixels: 0 },
+  { name: 'Chest', color: [238, 238, 238], coloredPixels: 0, totalPixels: 0, originalTotalPixels: 0 },
+];
 
 window.onload = function () {
   document.getElementById("color-picker").value = brushColor;
@@ -39,33 +45,78 @@ function preload() {
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background("#D1E1FF");
-  image(bodyImage, 0, 0, 600, 700);
+  image(bodyImage, 0, 0, 300, 700);
   mask = createGraphics(width, height);
-  mask.image(bodyImage, 0, 0, 600, 700);
-
-  totalPixels = 0;
-  loadPixels();
-  totalPixels = 0;
-  const tolerance = 20;
-
-  for (let i = 0; i < pixels.length; i += 4) {
-    let pixelR = pixels[i];
-    let pixelG = pixels[i + 1];
-    let pixelB = pixels[i + 2];
-    let pixelA = pixels[i + 3];
-
-    if (
-      Math.abs(pixelR - 238) <= tolerance &&
-      Math.abs(pixelG - 238) <= tolerance &&
-      Math.abs(pixelB - 238) <= tolerance &&
-      pixelA === 255
-    ) {
-      totalPixels++;
-    }
-  }
-  updatePixels();
+  mask.image(bodyImage, 0, 0, 300, 700);
+  calculateOriginalTotalPixels();
+  calculateTotalPixels();
+  logTotalPixels();
 }
 
+function calculateOriginalTotalPixels() {
+  const tolerance = 10;
+
+  for (const bodyPart of bodyParts) {
+    let partPixels = 0;
+
+    for (let i = 0; i < bodyImage.width; i++) {
+      for (let j = 0; j < bodyImage.height; j++) {
+        const pixelColor = bodyImage.get(i, j);
+
+        if (
+          Math.abs(pixelColor[0] - bodyPart.color[0]) <= tolerance &&
+          Math.abs(pixelColor[1] - bodyPart.color[1]) <= tolerance &&
+          Math.abs(pixelColor[2] - bodyPart.color[2]) <= tolerance
+        ) {
+          partPixels++;
+        }
+      }
+    }
+
+    bodyPart.originalTotalPixels = partPixels;
+  }
+}
+
+function calculateTotalPixels() {
+  const tolerance = 10;
+
+  for (const bodyPart of bodyParts) {
+    let partPixels = 0;
+
+    for (let i = 0; i < bodyImage.width; i++) {
+      for (let j = 0; j < bodyImage.height; j++) {
+        const pixelColor = bodyImage.get(i, j);
+
+        if (
+          Math.abs(pixelColor[0] - bodyPart.color[0]) <= tolerance &&
+          Math.abs(pixelColor[1] - bodyPart.color[1]) <= tolerance &&
+          Math.abs(pixelColor[2] - bodyPart.color[2]) <= tolerance
+        ) {
+          partPixels++;
+        }
+      }
+    }
+
+    bodyPart.totalPixels = partPixels;
+  }
+}
+
+function calculateRemainingPixels() {
+  for (const bodyPart of bodyParts) {
+    const remainingPixels = bodyPart.originalTotalPixels - bodyPart.totalPixels;
+    bodyPart.remainingPixels = remainingPixels;
+
+    console.log(`Remaining pixels for ${bodyPart.name}:`, remainingPixels);
+    console.log(`Original pixels for ${bodyPart.name}:`, bodyPart.originalTotalPixels);
+    console.log(`Total pixels for ${bodyPart.name}:`, bodyPart.totalPixels);
+  }
+}
+
+function logTotalPixels() {
+  bodyParts.forEach((part) => {
+    console.log(`Total pixels for ${part.name}:`, part.totalPixels);
+  });
+}
 
 function draw() {
   if (mouseIsPressed) {
@@ -88,8 +139,6 @@ function marker() {
   circle(mouseX, mouseY, radius);
 
 
-  let tolerance = 10;
-
   let brushR = red(color(brushColor));
   let brushG = green(color(brushColor));
   let brushB = blue(color(brushColor));
@@ -100,36 +149,32 @@ function marker() {
     let pixelG = pixels[i + 1];
     let pixelB = pixels[i + 2];
 
-    if (
-      Math.abs(pixelR - brushR) <= tolerance &&
-      Math.abs(pixelG - brushG) <= tolerance &&
-      Math.abs(pixelB - brushB) <= tolerance
-    ) {
+    // Check if the pixel matches the brush color
+    if (pixelR === brushR && pixelG === brushG && pixelB === brushB) {
       coloredPixels++;
     }
   }
   updatePixels();
-
 }
 
-function calculateColoredPercentage() {
-  let percentage = (coloredPixels / totalPixels) * 100;
+function calculateColoredPercentage(part) {
+  let percentage = (part.coloredPixels / part.totalPixels) * 100;
   percentage = Math.min(percentage, 100);
-  return percentage.toFixed(1);
+  return percentage.toFixed(1) + '%';
 }
 
 const bsa = document.getElementById("bsa");
+
 function calculateSurfaceArea() {
-  coloredPixels = 0;
-  marker();
-  let coloredPercentage = calculateColoredPercentage();
-  bsa.innerHTML = "Area Percentage: " + coloredPercentage + "%";
+  calculateTotalPixels();
+  calculateRemainingPixels();
+  populateTable();
 }
 
 // Clear the canvas
 function clearCanvas() {
   background("#D1E1FF");
-  image(bodyImage, 0, 0, 600, 700);
+  image(bodyImage, 0, 0, 300, 700);
 
   coloredPixels = 0;
   totalPixels = 0;
@@ -178,26 +223,31 @@ const tableData = [
   { bodyPart: 'Left Arm', percentage: '0%', ftu: '0', tcs: '0' },
 ];
 
+function updateTable() {
+  calculateTotalPixels();
+  calculateRemainingPixels();
+  populateTable();
+}
+
 // Function to populate the table
 function populateTable() {
   const tableBody = document.querySelector('#data-table tbody');
 
-  tableData.forEach((row) => {
+  tableBody.innerHTML = '';
+
+  bodyParts.forEach((part) => {
     const newRow = document.createElement('tr');
     const bodyPartCell = document.createElement('td');
     const percentageCell = document.createElement('td');
-    const ftuCell = document.createElement('td');
-    const tcsCell = document.createElement('td');
 
-    bodyPartCell.textContent = row.bodyPart;
-    percentageCell.textContent = row.percentage;
-    ftuCell.textContent = row.ftu;
-    tcsCell.textContent = row.tcs;
+    const remainingPercentage = (part.remainingPixels / part.originalColoredPixels) * 100;
+    part.remainingPercentage = Math.max(0, remainingPercentage).toFixed(1) + '%';
+
+    bodyPartCell.textContent = part.name;
+    percentageCell.textContent = part.remainingPercentage;
 
     newRow.appendChild(bodyPartCell);
     newRow.appendChild(percentageCell);
-    newRow.appendChild(ftuCell);
-    newRow.appendChild(tcsCell);
 
     tableBody.appendChild(newRow);
   });
@@ -205,4 +255,3 @@ function populateTable() {
 
 // Call the populateTable function when the page loads
 window.addEventListener('DOMContentLoaded', populateTable);
-
